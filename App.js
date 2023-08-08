ab1d737f284a6e20dffea789b0e855c8364536dd
Atikorn <264318241004-st@rmutsb.ac.th>
2023-08-08 12:15:13 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import react from 'react';
+/*import ArrayMap from './array-map';*/
+/*import ColcumItem from './colume-item';*/
+/*import RowItem from './row-item';*/
+/*import LayoutItem from './layout-item';
+import Gg from './GG';
+import FourItem from './four';
+import { View } from 'react-native';*/
+/*import ButtonDemo from './button-demo';*/
+/*import ButtonStateDemo from './button-state-demo';*/
+import TextInputDemo2 from './text-input-demo2';
+
+export default function App() {
+  return (
+        /*[<Gg/>,<LayoutItem/>,<FourItem/>]*/
+        /*<ButtonDemo/>*/
+        <TextInputDemo2/>
+  );
+}
